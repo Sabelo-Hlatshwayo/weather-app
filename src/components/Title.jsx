9bb81d53c44fbe090dde94e784e69be680697f6a
Sabelo Hlatshwayo <evidence.hlatshwayo@gmail.com>
2023-01-02 19:46:03 +0200

    Add the functionality for toggling between the light theme and the dark theme on the title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { useTheme } from "../contexts/ThemeContext";
 
 function Title() {
+    const { theme } = useTheme();
+
+    const themeStyle = (theme) => {
+        return {
+            color: theme === "dark" ? "#fff" : "#000",
+        };
+    };
+
     return (
-        <h1 className="capitalize text-black text-3xl font-bold">
+        <h1
+            style={themeStyle(theme)}
+            className="capitalize text-black text-3xl font-bold"
+        >
             the weather app
         </h1>
     );
